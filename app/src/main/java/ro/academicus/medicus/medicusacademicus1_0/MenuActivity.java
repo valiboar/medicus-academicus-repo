c30a343f611700736b1df894b48cd386e6d691b2
valiboar <vali.boar@yahoo.com>
2016-04-28 14:58:38 +0300

    Update Menu interface + login functionality

--- app/src/main/java/ro/academicus/medicus/medicusacademicus1_0/MenuActivity.java
+++ app/src/main/java/ro/academicus/medicus/medicusacademicus1_0/MenuActivity.java
@@ -1,6 +1,7 @@
 package ro.academicus.medicus.medicusacademicus1_0;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -16,13 +17,19 @@ public class MenuActivity extends AppCompatActivity {
         Intent intent = getIntent();
         String patientName = intent.getStringExtra(LoginActivity.EXTRA_PATIENT_NAME);
 
-        TextView textView = (TextView) findViewById(R.id.welcome_text_view);
-        textView.append(",\n" + patientName + "!");
-        textView.setTextSize(40);
+        TextView welcomeTextView = (TextView) findViewById(R.id.welcome_text_view);
+
+        Typeface typeFace = Typeface.createFromAsset(getAssets(),"fonts/Montserrat-Bold.ttf");
+        welcomeTextView.setTypeface(typeFace);
+
+
+        welcomeTextView.append(",\n" + patientName + "!");
+        welcomeTextView.setTextSize(40);
     }
 
     public void signOut(View view) {
         Intent intent = new Intent(this, LoginActivity.class);
+        MenuActivity.this.finish();
         startActivity(intent);
     }
 }
